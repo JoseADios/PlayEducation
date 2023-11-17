d33fedd0e0b0881e6632fcf820b0bf57c1949444
EudisContreras <eudiscontreras1329@gmail.com>
2023-11-17 14:58:24 -0400

    Modelo Docente

---
+++ app/Http/Controllers/DocenteController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Docente;
+use Illuminate\Http\Request;
+
+class DocenteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Docente $docente)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Docente $docente)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Docente $docente)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Docente $docente)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_11_17_182343_create_docentes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('docentes', function (Blueprint $table) {
+            $table->id();
+            $table->string('password_temp')->default();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('correo')->unique();
+            $table->string('usuario')->unique();
+            $table->string('password');
+            $table->date('fecha_nacimiento')->nullable();
+            $table->string('ruta_imagen')->nullable();
+            $table->boolean('estado')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('docentes');
+    }
+};
