d2652e77ffc5fc40445d62f73798eb44176e29d9
José A. de Dios <josedios10@gmail.com>
2023-11-17 14:15:24 -0400

    Model centro educativo

---
+++ app/Models/CentroEducativo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CentroEducativo extends Model
+{
+    use HasFactory;
+}
