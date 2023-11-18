b79de74435b686ed5db00dea1948dba211f68e21
José A. de Dios <josedios10@gmail.com>
2023-11-17 20:17:00 -0400

    Modelo logros

---
+++ database/migrations/2023_11_18_001112_create_estudiantes_logros_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('estudiantes_logros', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('estudiante_id')->constrained('estudiantes');
+            $table->foreignId('logro_id')->constrained('logros');
+            $table->dateTime('fecha_obtencion');
+            $table->boolean('activo')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('estudiantes_logros');
+    }
+};
