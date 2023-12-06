1bbddea20447715024e85ca2ff08e286aa1378cb
José A. de Dios <josedios10@gmail.com>
2023-12-05 22:42:48 -0400

    Update login page design

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -5,16 +5,13 @@
                 <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
                     <div class="card card-plain mt-8">
                         <div class="card-header pb-0 text-left bg-transparent">
-                            <h3 class="font-weight-bolder text-info text-gradient">{{ __('Welcome back') }}</h3>
-                            <p class="mb-0">{{ __('Create a new acount')}}<br></p>
-                            <p class="mb-0">{{__('OR Sign in with these credentials:') }}</p>
-                            <p class="mb-0">{{ __('Email ') }}<b>{{ __('admin@softui.com') }}</b></p>
-                            <p class="mb-0">{{ __('Password ') }}<b>{{ __('secret') }}</b></p>
+                            <h3 class="font-weight-bolder text-info text-gradient">{{ __('Mestros') }}</h3>
+                            <p class="mb-0">{{ __('Inicio de sesión')}}<br></p>
                         </div>
                         <div class="card-body">
                             <form wire:submit.prevent="login" action="#" method="POST" role="form text-left">
                                 <div class="mb-3">
-                                    <label for="email">{{ __('Email') }}</label>
+                                    <label for="email">{{ __('Correo') }}</label>
                                     <div class="@error('email')border border-danger rounded-3 @enderror">
                                         <input wire:model="email" id="email" type="email" class="form-control"
                                             placeholder="Email" aria-label="Email" aria-describedby="email-addon">
@@ -22,7 +19,7 @@
                                     @error('email') <div class="text-danger">{{ $message }}</div> @enderror
                                 </div>
                                 <div class="mb-3">
-                                    <label for="password">{{ __('Password') }}</label>
+                                    <label for="password">{{ __('Contraseña') }}</label>
                                     <div class="@error('password')border border-danger rounded-3 @enderror">
                                         <input wire:model="password" id="password" type="password" class="form-control"
                                             placeholder="Password" aria-label="Password"
@@ -33,22 +30,22 @@
                                 <div class="form-check form-switch">
                                     <input wire:model="remember_me" class="form-check-input" type="checkbox"
                                         id="rememberMe">
-                                    <label class="form-check-label" for="rememberMe">{{ __('Remember me') }}</label>
+                                    <label class="form-check-label" for="rememberMe">{{ __('Recordarme') }}</label>
                                 </div>
                                 <div class="text-center">
                                     <button type="submit"
-                                        class="btn bg-gradient-info w-100 mt-4 mb-0">{{ __('Sign in') }}</button>
+                                        class="btn bg-gradient-info w-100 mt-4 mb-0">{{ __('Iniciar sesion') }}</button>
                                 </div>
                             </form>
                         </div>
                         <div class="card-footer text-center pt-0 px-lg-2 px-1">
-                            <small class="text-muted">{{ __('Forgot you password? Reset you password') }} <a
+                            <small class="text-muted">{{ __('Olvidaste tu contraseña? Reiniciala') }} <a
                                     href="{{ route('forgot-password') }}"
-                                    class="text-info text-gradient font-weight-bold">{{ __('here') }}</a></small>
+                                    class="text-info text-gradient font-weight-bold">{{ __('aquí') }}</a></small>
                             <p class="mb-4 text-sm mx-auto">
-                                {{ __(' Don\'t have an account?') }}
+                                {{ __(' No tienes una cuenta?') }}
                                 <a href="{{ route('sign-up') }}"
-                                    class="text-info text-gradient font-weight-bold">{{ __('Sign up') }}</a>
+                                    class="text-info text-gradient font-weight-bold">{{ __('Registrarse') }}</a>
                             </p>
                         </div>
                     </div>
