637b7a1eb95ae5020999916746b47ec5da643c26
José A. de Dios <josedios10@gmail.com>
2023-12-06 13:13:51 -0400

    Estilo login

--- resources/views/livewire/login-estudiantes.blade.php
+++ resources/views/livewire/login-estudiantes.blade.php
@@ -1,4 +1,4 @@
-<div>
+{{-- <div>
     <form method="POST" action="/login-estudiante">
         @csrf
         <label for="usuario">Usuario:</label>
@@ -12,4 +12,96 @@
         @endif
         <button type="submit">Iniciar sesión</button>
     </form>
-</div>
+</div> --}}
+
+
+<head>
+    <!-- Nucleo Icons -->
+    <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link id="pagestyle" href="{{ asset('assets/css/soft-ui-dashboard.css?v=1') }}" rel="stylesheet" />
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <!--   Core JS Files   -->
+    <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
+    <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="{{ asset('assets/js/soft-ui-dashboard.js') }}"></script>
+</head>
+
+
+<section>
+    <div class="page-header section-height-75">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
+                    <div class="card card-plain mt-8">
+                        <div class="card-header pb-0 text-left bg-transparent">
+                            <h3 class="font-weight-bolder text-info text-gradient">{{ __('Estudiantes') }}</h3>
+                            <p class="mb-0">{{ __('Inicio de sesión') }}<br></p>
+                        </div>
+                        <div class="card-body">
+                            <form action="/login-estudiante" method="POST" role="form text-left">
+                                @csrf
+
+                                <div class="mb-3">
+                                    <label for="email">{{ __('Correo') }}</label>
+                                    <div class="@error('email')border border-danger rounded-3 @enderror">
+                                        <input id="email" type="email" class="form-control" placeholder="Email"
+                                            aria-label="Email" aria-describedby="email-addon">
+                                    </div>
+                                    @error('email')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                <div class="mb-3">
+                                    <label for="password">{{ __('Contraseña') }}</label>
+                                    <div class="@error('password')border border-danger rounded-3 @enderror">
+                                        <input id="password" type="password" class="form-control"
+                                            placeholder="Password" aria-label="Password"
+                                            aria-describedby="password-addon">
+                                    </div>
+                                    @error('password')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                @if ($errors->any())
+                                    <div class="text-danger">
+                                        {{ $errors->first('error') }}
+                                    </div>
+                                @endif
+                                {{-- <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox"
+                                        id="rememberMe">
+                                    <label class="form-check-label" for="rememberMe">{{ __('Recordarme') }}</label>
+                                </div> --}}
+                                <div class="text-center">
+                                    <button type="submit"
+                                        class="btn bg-gradient-info w-100 mt-4 mb-0">{{ __('Iniciar sesion') }}</button>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                            <p class="mb-2 text-sm mx-auto">
+                                {{ __(' No sabes tu cuenta?') }}
+                            <div class="text-info text-gradient font-weight-bold">
+                                {{ __('Pide los datos a tu maestro') }}</div>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
+                        <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6"
+                            style="background-image:url('../assets/img/curved-images/curved6.jpg')"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
