0f90a3e4efd0eaa0bc10295bfda174b90a8e2868
joseDios <josedios10@gmail.com>
2024-02-24 13:39:14 -0400

    prueba, cambio de variable con funcion

--- app/Http/Livewire/PruebaComp.php
+++ app/Http/Livewire/PruebaComp.php
@@ -8,6 +8,10 @@ class PruebaComp extends Component
 {
     public $prueba;
 
+    public function setData($data) {
+        $this->prueba = $data;
+    }
+
     public function render()
     {
         return view('livewire.prueba-comp');
--- resources/views/livewire/prueba-comp.blade.php
+++ resources/views/livewire/prueba-comp.blade.php
@@ -5,14 +5,14 @@
     <p>Mirame</p>
 
     <input onclick="passData()" type="button" value="Jalar data">
-    <input id="inpt" type="text" value="{{ $prueba }}">
+    <input id="inpt" type="text" value="">
     <textarea name="" id="text-a" cols="30" rows="10">{{ $prueba }}</textarea>
 
     <script>
         function passData() {
             let el = document.getElementById('inpt').value
 
-            @this.prueba = el
+            @this.setData(el)
 
             let pp = @this.prueba
 
